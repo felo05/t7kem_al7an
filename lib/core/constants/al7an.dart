b38c64af57a063fb4df13cee6a84f8c4af4027be
Felopatear Alaa <feloalaa50@gmail.com>
2026-07-10 02:19:36 +0300

    new al7an

--- lib/core/constants/al7an.dart
+++ lib/core/constants/al7an.dart
@@ -8,80 +8,88 @@ class L7n{
 
 class Al7an {
   static List<L7n> kg1 = [
-   L7n(name: "مرد انجيل رفع بخور عشية السنوى (جمفثمارؤوت)"),
-    L7n(name: "سوتيس امين دمج"),
-    L7n(name: "مرد مزمور عيد القيامة"),
-    L7n(name: "ختام الصلوات عيد القيامة"),
+    L7n(name: "مرد انجيل رفع بخور عشية السنوى (جمفثمارؤوت)",pdfUrl: "https://drive.google.com/file/d/1CLBzRrfotPu6aw-WdYoUDIEFOYUJbTqN/view?usp=drive_link"),
+    L7n(name: "سوتيس امين دمج",pdfUrl: "https://drive.google.com/file/d/1kc2S8Ed0nol7u2oYdTuACImyTHGf8nqT/view?usp=drive_link"),
+    L7n(name:"ختام الصلوات عيد الصليب",pdfUrl:"https://drive.google.com/file/d/1olaZiM0_rCisD_o-gaF8URKwzqLeR01H/view?usp=drive_link"),
+    L7n(name: "مرد مزمور عيد القيامة",pdfUrl: "https://drive.google.com/file/d/1ES0Xna8CY0onIwoz2yBySpzeCcmtcFFA/view?usp=drive_link"),
+    L7n(name: "ختام الصلوات عيد القيامة",pdfUrl: "https://drive.google.com/file/d/1YIQy_2i8IQD69X2Bjrle0b6er1TJjZ4i/view?usp=drive_link"),
   ];
   static List<L7n> kg2 = [
     kg1[0],
     kg1[1],
-    L7n(name: "مرد المجمع (ارابواسمو)"),
-    L7n(name: "مرد انجيل القداس سنوي (اونياطو)"),
-    L7n(name: "هيتينية القيامة للملاك ميخائيل"),
-    L7n(name: "مرد التوزيع لعيد القيامة (الطريقة السريعة)"),
+    kg1[2],
+    L7n(name: "مرد المجمع (ارابواسمو)",pdfUrl: "https://drive.google.com/file/d/1es0E2fZFfXyNO51eYU9Qmrh8fVFe4Fd5/view?usp=drive_link"),
+    L7n(name: "مرد انجيل القداس سنوي (اونياطو)",pdfUrl: "https://drive.google.com/file/d/18dhfXYi5LdEkXNptWx0HJpZnDJFBVS-r/view?usp=drive_link"),
+    L7n(name: "هيتينية القيامة للملاك ميخائيل",pdfUrl: "https://drive.google.com/file/d/1s-b5otASw5lkaUeE2q3OAjXyLXUeYtfV/view?usp=drive_link"),
+    L7n(name: "مرد التوزيع لعيد القيامة (الطريقة السريعة)",pdfUrl: "https://drive.google.com/file/d/1J2pB1hq6dnRMd6dIoh9Ax-mV8J7yYlpE/view?usp=drive_link"),
   ];
   static List<L7n> kg3 = [
-    L7n(name: "لحن البركة"),
-    L7n(name: "مقدمة الذكصولوجيات الفرايحي (الطريقة الوسط)"),
-    L7n(name: "اجيوس الفرايحي الكبير"),
+    L7n(name: "لحن البركة",pdfUrl:"https://drive.google.com/file/d/1UnUQD1z9HL94mpkGpkCI89OKjJ45WnEi/view?usp=drive_link"),
+    L7n(name: "مقدمة الذكصولوجيات الفرايحي (الطريقة الوسط)",pdfUrl:"https://drive.google.com/file/d/1q06wNY7IX6rUnFm-v6yU51Ul-yMBWUXF/view?usp=drive_link"),
+    L7n(name: "اجيوس الفرايحي الكبير",pdfUrl: "https://drive.google.com/file/d/1QFG-UEs2OxHITArdivzZ8YOulns-CuW_/view?usp=drive_link"),
   ];
   static List<L7n> ola1 = [
-    L7n(name: "الليلويا فاي بابي"),
-    L7n(name: "ختام التسبحة (افنوتي نان)"),
-    L7n(name: "مرد انجيل عيد القيامة (اللليلويا)"),
-    L7n(name: "جمفثمارؤوت صيام الرسل"),
+    L7n(name: "الليلويا فاي بابي",pdfUrl: "https://drive.google.com/file/d/11SphQPXMwmUpJIHaEG5FkLrdxfAizVld/view?usp=drive_link"),
+    L7n(name: "ختام التسبحة (افنوتي نان)",pdfUrl: "https://drive.google.com/file/d/1wf_ZLYgQ529dLDfOXZWl9QFS_Cv2uJ2L/view?usp=drive_link"),
+    L7n(name: "مرد دورة الصليب",pdfUrl: "https://drive.google.com/file/d/1agQF7SHkNfsYLbjoEjfNyUEgMh-oD8vS/view?usp=drive_link"),
+    L7n(name: "مرد انجيل عيد القيامة (اللليلويا)",pdfUrl: "https://drive.google.com/file/d/1kATYESCEuOME2jgG4ufuyGY0t8gZIpN2/view?usp=drive_link"),
+    L7n(name: "جمفثمارؤوت صيام الرسل",pdfUrl: "https://drive.google.com/file/d/11jSPFq0djLuhbbUAPpbUUy9zptWMblb4/view?usp=drive_link"),
   ];
   static List<L7n> ola2 = [
     ola1[0],
     ola1[1],
-    L7n(name: "الليلويا جي افمفئي"),
-    L7n(name: "ذكصوبوجية مارمرقس"),
-    L7n(name: "ارباع الناقوس فرايحي لعيد القيامة (اول 3 اربع فقط)"),
-    L7n(name: "مرد انجيل عيد القيامة (ليبون)"),
+    ola1[2],
+    L7n(name: "الليلويا جي افمفئي",pdfUrl: "https://drive.google.com/file/d/1pr9BZcYzwa83dInGBYEXzRrWupRQ5Zl0/view?usp=drive_link"),
+    L7n(name: "ذكصوبوجية مارمرقس",pdfUrl: "https://drive.google.com/file/d/1EUP3U_aU6_VE45YS6yR1qjUnvwmO7ccV/view?usp=drive_link"),
+    L7n(name: "ارباع الناقوس فرايحي لعيد القيامة (اول 3 اربع فقط)",pdfUrl: "https://drive.google.com/file/d/1ctrD-nvqGBnPNPuUXqv9RcFNWglph6Nv/view?usp=drive_link"),
+    L7n(name: "مرد انجيل عيد القيامة (ليبون)",pdfUrl: "https://drive.google.com/file/d/1ZMA5_bzMQt5OcOanVXp0n2jI2iXrlkNs/view?usp=drive_link"),
   ];
   static List<L7n> ola3 = [
-    L7n(name: "الاسبزمس الواطس السنوي (ابتوشيس افنوتي)"),
-    L7n(name: "كاطانى خوروس التوزيع"),
-    L7n(name: "بخرستوس افطونف عيد القيامة"),
+    L7n(name: "الاسبزمس الواطس السنوي (ابتوشيس افنوتي)",pdfUrl: "https://drive.google.com/file/d/1JSCJ-R5oe2celFPxzUffmdMcoyunt9n_/view?usp=drive_link"),
+    L7n(name: "كاطانى خوروس التوزيع",pdfUrl: "https://drive.google.com/file/d/1EiFb7gx9NVPg8fB9iCQOywEurHxHc1ls/view?usp=drive_link"),
+    L7n(name: "بخرستوس افطونف عيد القيامة",pdfUrl: "https://drive.google.com/file/d/1ZHfDGokJGbH388DP145pje6Xn7MEiEjw/view?usp=drive_link"),
   ];
   static List<L7n> talta1 = [
-    L7n(name: "ذكصولوجية الرسل"),
-    L7n(name: "تي شوري"),
-    L7n(name: "طون سينا"),
-    L7n(name: "مقدمة بى ابنفما"),
+    L7n(name: "ذكصولوجية الرسل",pdfUrl: "https://drive.google.com/file/d/1zr62s5_8krwQazjFpbnPZoRa33eQdU_h/view?usp=drive_link"),
+    L7n(name: "تي شوري",pdfUrl: "https://drive.google.com/file/d/1P93ipKsGu5daL1bNXUFYORVeFQvDFvzO/view?usp=drive_link"),
+    L7n(name: "مرد انجيل عيد الصليب",pdfUrl: "https://drive.google.com/file/d/1z8YvqwIaAEeADcwXikjvJgKkdt-px1_N/view?usp=drive_link"),
+    L7n(name: "طون سينا",pdfUrl: "https://drive.google.com/file/d/1T6-sT2CjFJXroeQaVYZdQzXZBlmcg7wk/view?usp=drive_link"),
+    L7n(name: "مقدمة بى ابنفما",pdfUrl: "https://drive.google.com/file/d/1a-1fX0jTfn0pcNrtKfQEjwLRL2NeU0Gy/view?usp=drive_link"),
   ];
   static List<L7n> talta2 = [
     talta1[0],
     talta1[1],
-    L7n(name: "ذكصولوجية العذراء لرفع بخور عشية"),
-    L7n(name: "المزمور ال150 من الهوس الرابع"),
-    L7n(name: "باشويس عيد القيامة (المحير)"),
-    L7n(name: "مرد التوزيع لعيد القيامة (الطريقة الوسط)"),
+    talta1[2],
+    L7n(name: "ذكصولوجية العذراء لرفع بخور عشية",pdfUrl: "https://drive.google.com/file/d/1RXNqnSRhRvolNZU0L52DgOVQkCbI3bWU/view?usp=drive_link"),
+    L7n(name: "المزمور ال150 من الهوس الرابع",pdfUrl: "https://drive.google.com/file/d/1SOOowpl8OV3RUp-2HxjRb6JJ1JAnIh2l/view?usp=drive_link"),
+    L7n(name: "باشويس عيد القيامة (المحير)",pdfUrl: "https://drive.google.com/file/d/1LOiHl_T5HxPjeCseNIHkpcKSL4VhOuvA/view?usp=drive_link"),
+    L7n(name: "مرد التوزيع لعيد القيامة (الطريقة الوسط)",pdfUrl: "https://drive.google.com/file/d/1Duu3tJwKKxWGMzvNpPwU5UNVJHxhaaMI/view?usp=drive_link"),
   ];
   static List<L7n> talta3 = [
-    L7n(name: "مرد الابركسيس السنوي الكبير بالتكملة"),
-    L7n(name: "اللى التوزيع الكبير لعيد القيامو"),
-    L7n(name: "المزمور السنجاري الكبير لعيد القيامة"),
+    L7n(name: "مرد الابركسيس السنوي الكبير بالتكملة",pdfUrl: "https://drive.google.com/file/d/1XjPKjXaaFfPg7orBf91AxuEEPrSDUl7T/view?usp=drive_link"),
+    L7n(name: "اللى التوزيع الكبير لعيد القيامة",pdfUrl: "https://drive.google.com/file/d/1VmHU05vg8XPfKTgCqcr0YWLiVVhH66-P/view?usp=drive_link"),
+    L7n(name: "المزمور السنجاري الكبير لعيد القيامة",pdfUrl: "https://drive.google.com/file/d/1HAt9MMZrCvo4-00A1LdHnh03wq82RZjL/view?usp=drive_link"),
   ];
   static List<L7n> khamsa1 = [
-    L7n(name: "ذكصولوجية العذراء لرفع بخور باكر"),
-    L7n(name: "مرد الابركسيس السنوي"),
-    L7n(name: "ذكصولوجية عيد القيامة"),
-    L7n(name: "مرد قطع الساعة الثالثة"),
+    L7n(name: "ذكصولوجية العذراء لرفع بخور باكر",pdfUrl: "https://drive.google.com/file/d/1KALK28c9J1MHRkll-RLfQzXFU1tgwvNA/view?usp=drive_link"),
+    L7n(name: "مرد الابركسيس السنوي",pdfUrl: "https://drive.google.com/file/d/1kHkocOHpphAZA-flAVxQO4PXhyttN4bW/view?usp=drive_link"),
+    L7n(name: "ارباع الناقوس لعيد الصليب",pdfUrl: "https://drive.google.com/file/d/1l6cWi3cMfx68vFc0jr_AxmpmKWWTH8cM/view?usp=drive_link"),
+    L7n(name: "ذكصولوجية عيد القيامة",pdfUrl: "https://drive.google.com/file/d/1vP35uu9Jixfwh2R5X1gQcZMkz9HrQWrO/view?usp=drive_link"),
+    L7n(name: "مرد قطع الساعة الثالثة",pdfUrl: "https://drive.google.com/file/d/1ZdLn6bBZnZYb8pLXcKvuVfY641-qMIeX/view?usp=drive_link"),
   ];
   static List<L7n> khamsa2 = [
     khamsa1[0],
     khamsa1[1],
-    L7n(name: "ذكصولوجية السمائيين"),
-    L7n(name: "مردات الانافورة باسيلي وغريغوري"),
-    L7n(name: "ارباع الناقوس لعيد القيامة (اول 3 ارباع كبار + العذراء والملاك ميخائيل + الختام)"),
-    L7n(name: "مردالابركسيس لعيد القيامة"),
+    khamsa1[2],
+    L7n(name: "ذكصولوجية السمائيين",pdfUrl: "https://drive.google.com/file/d/1189tzYleGB9vC1DOs14NP7SqOwrgtai2/view?usp=drive_link"),
+    L7n(name: "مردات الانافورة باسيلي وغريغوري",pdfUrl: "https://drive.google.com/file/d/1aUomkCc2uhNY14yu04OFKoROlGI9WtDI/view?usp=drive_link"),
+    L7n(name: "ارباع الناقوس لعيد القيامة (اول 3 ارباع كبار + العذراء والملاك ميخائيل + الختام)",pdfUrl: "https://drive.google.com/file/d/1bKkfZIxUpJDvcvqlaXX2TCtQiYQfkCkV/view?usp=drive_link"),
+    L7n(name: "مردالابركسيس لعيد القيامة",pdfUrl: "https://drive.google.com/file/d/1ZigLpsS48KYW598thLUXCkAxKnNdVt62/view?usp=drive_link"),
   ];
   static List<L7n> khamsa3 = [
-    L7n(name: "اسبازستي الكبير"),
-    L7n(name: "بي ابنفما + البرلكس + المحير"),
-    L7n(name: "البولس الفرايحي"),
+    L7n(name: "اسبازستي الكبير",pdfUrl: "https://drive.google.com/file/d/1m52yC-zI4gQ98jXnlD3CJ34EBFuzhdNy/view?usp=drive_link"),
+    L7n(name: "بي ابنفما + البرلكس + المحير",pdfUrl: "https://drive.google.com/file/d/1QugzoPasIl0l72BGfgDG_pZPyZDXeml7/view?usp=drive_link"),
+    L7n(name: "البولس الفرايحي",pdfUrl: "https://drive.google.com/file/d/16T6uLh6lpMtWZuGa97hbKRY1o2R1bLGe/view?usp=drive_link"),
   ];
   static const String df = "دف";
   static const String treanto = "تريانتو";
